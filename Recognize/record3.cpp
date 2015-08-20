1a5306823b5f88cdba8989943cca1cb0bd0523e7
SunnerLi <a6214123@gmail.com>
2015-08-20 06:40:26 +0000

    final edition

--- Recognize/record3.cpp
+++ Recognize/record3.cpp
@@ -6,11 +6,24 @@ using namespace std;
 
 int imageCounter = 1, timer = 0;
 
+void delayDefineByMyself(float time_want_to_wait)
+{
+	clock_t bef=0, aft=0;
+	bef=clock();
+	aft=clock()-bef;
+	float diff=((int)aft)/CLOCKS_PER_SEC;
+	while(diff<time_want_to_wait)
+	{
+		aft=clock()-bef;
+		diff=((float)aft)/CLOCKS_PER_SEC;
+	}
+}
+
+
 void Decide_Image_Name(IplImage* image){
-	char *s;
-	cout<<imageCounte<<endl;
+	char s[50];// need to use array, or get segmentation fault
+	cout<<imageCounter<<endl;
 	sprintf(s, "%06d.png", imageCounter++);
-	cout<<imageCounte<<endl;
 	cvSaveImage(s, image, 0);
 }
 
@@ -18,12 +31,12 @@ void Capture_Image(IplImage* image, CvCapture* capture){
 	int i;
 	char *s;
 	while( imageCounter < NUMBER_OF_IMAGE ){
+		delayDefineByMyself(1);
 		image = cvQueryFrame(capture);
-		cvWaitKey(10000);
+		cvWaitKey(1000);
 		timer++;
 		if( timer == 10){
 			timer = 0;
-			cout<<"capture!"<<endl;
 			Decide_Image_Name(image);
 		}
 	}
@@ -45,9 +58,10 @@ int main(){
 		cout<<"camera is null..."<<endl;
 
 	//work for a while
+	//delayDefineByMyself(30);
 	for(i=0; i<15; i++){
 		if( img = cvQueryFrame(capture) )
-			cvWaitKey(2000);
+			cvWaitKey(20);
 		else
 			cout<<"cannot capture the image..."<<endl;
 	}
