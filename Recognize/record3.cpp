59f3c27775c467e98fd29960a18d232d96a59569
sunner <a6214123@gmail.com>
2015-08-20 14:01:58 +0800

    record3 transfer to record3.cpp

---
+++ Recognize/record3.cpp
@@ -0,0 +1,59 @@
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+#include <iostream>
+#define NUMBER_OF_IMAGE 20
+using namespace std;
+
+int imageCounter = 1, timer = 0;
+
+void Decide_Image_Name(IplImage* image){
+	char *s;
+	cout<<imageCounte<<endl;
+	sprintf(s, "%06d.png", imageCounter++);
+	cout<<imageCounte<<endl;
+	cvSaveImage(s, image, 0);
+}
+
+void Capture_Image(IplImage* image, CvCapture* capture){
+	int i;
+	char *s;
+	while( imageCounter < NUMBER_OF_IMAGE ){
+		image = cvQueryFrame(capture);
+		cvWaitKey(50);
+		timer++;
+		if( timer == 10){
+			timer = 0;
+			cout<<"capture!"<<endl;
+			Decide_Image_Name(image);
+		}
+	}
+}
+
+int main(){
+	//variable declaration
+	IplImage *img;
+	int i;
+	CvCapture *capture = 0;
+
+	//assign the property about camera
+	capture = cvCaptureFromCAM(0);
+	if( capture ){
+		cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH, 320);
+		cvSetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT, 240);
+	}
+	else
+		cout<<"camera is null..."<<endl;
+
+	//work for a while
+	for(i=0; i<15; i++){
+		if( img = cvQueryFrame(capture) )
+			cvWaitKey(20);
+		else
+			cout<<"cannot capture the image..."<<endl;
+	}
+
+	//capture Image
+	Capture_Image(img, capture);
+
+	return 0;
+}
